30cfce80593a028fd5bd9e0fabfb7bc2b8f61058
CesarPalma11 <cesarpalmag16@gmail.com>
2023-11-09 00:04:11 -0300

    models

--- src/pages/AllModels.jsx
+++ src/pages/AllModels.jsx
@@ -5,9 +5,65 @@ const AllModels = () => {
     <>
     <div className='container_all'>
       <h1>Todos los modelos</h1>
-      <div className='container__filter'>
-      <h3>Mostrar filtros</h3>
+      {/* A1 */}
+      <div className='section_A1'>
+        <img src='./img/AudiA1_Left.png' className='img_left' alt=''></img>
+        <h3>A1 Sportback</h3>
+        <img src='./img/audiA1.png' className='img_right' alt=''></img>
+        <button className='button_A1'>Saber más</button>
       </div>
+      {/* A3 */}
+      <div className='section_A1'>
+        <img src='./img/AudiA3_Left.png' className='img_left' alt=''></img>
+        <h3>A3 Sedán</h3>
+        <img src='./img/audiA3.png' className='img_right' alt=''></img>
+        <button className='button_A1'>Saber más</button>
+        <h3>A3 Sportback</h3>
+        <img src='./img/audiA3_Sportback.png' className='img_right' alt=''></img>
+        <button className='button_A1'>Saber más</button>
+      </div>
+      {/* A4 */}
+      <div className='section_A1'>
+        <img src='./img/AudiA4_Left.png' className='img_left' alt=''></img>
+        <h3>A4 Sedán</h3>
+        <img src='./img/audiA4.png' className='img_right' alt=''></img>
+        <button className='button_A1'>Saber más</button>
+      </div>
+      {/* A5 */}
+      <div className='section_A1'>
+        <img src='./img/AudiA5_Left.png' className='img_left' alt=''></img>
+        <h3>A5 Coupé</h3>
+        <img src='./img/audiA5.png' className='img_right' alt=''></img>
+        <button className='button_A1'>Saber más</button>
+        <h3>A5 Sportback</h3>
+        <img src='./img/audiA5_2.png' className='img_right' alt=''></img>
+        <button className='button_A1'>Saber más</button>
+      </div>
+      {/* A6 */}
+      <div className='section_A1'>
+        <img src='./img/AudiA6_Left.png' className='img_left' alt=''></img>
+        <h3>A6 Sedán</h3>
+        <img src='./img/audiA6.png' className='img_right' alt=''></img>
+        <button className='button_A1'>Saber más</button>
+        <h3>A6 allroad quattro</h3>
+        <img src='./img/audiA6_Quattro.png' className='img_right' alt=''></img>
+        <button className='button_A1'>Saber más</button>
+      </div>
+      {/* A7 */}
+      <div className='section_A1'>
+        <img src='./img/AudiA7_Left.png' className='img_left' alt=''></img>
+        <h3>A7 Sportback</h3>
+        <img src='./img/audiA7.png' className='img_right' alt=''></img>
+        <button className='button_A1'>Saber más</button>
+      </div>
+      {/* E-TRON */}
+      <div className='section_A1'>
+        <img src='./img/e-tron.png' className='img_left' alt=''></img>
+        <h3>RS e-tron GT</h3>
+        <img src='./img/e-tron1.png' className='img_right' alt=''></img>
+        <button className='button_A1'>Saber más</button>
+      </div>
+
     </div>
     </>
   )
